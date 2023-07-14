996236eb20f5f1fd20f8b542323582e414088899
nithin-sudarsan <nithinsudarsan740@gmail.com>
2023-07-14 14:31:34 +0530

    Initial commit

---
+++ src/test/java/com/example/DataMapperDemo/DataMapperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataMapperDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMapperDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
