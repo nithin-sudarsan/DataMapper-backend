5252b182c9318caae302f12413d59a786287dfc4
nithin-sudarsan <nithinsudarsan740@gmail.com>
2023-07-24 15:29:56 +0530

    Updated Dockerfile

---
+++ src/main/java/com/example/datamapperdemo/DataMapperDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.datamapperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMapperDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMapperDemoApplication.class, args);
+	}
+
+}
