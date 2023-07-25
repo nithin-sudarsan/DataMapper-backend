b2466623f1f9b4a75021697e9a561de28c097636
nithin-sudarsan <nithinsudarsan740@gmail.com>
2023-07-25 13:36:24 +0530

    Updated settings.xml

--- src/main/java/com/example/datamapperdemo/config/CorsFilter.java
+++ src/main/java/com/example/datamapperdemo/config/CorsFilter.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+        response.setHeader("Access-Control-Allow-Origin", "https://datamapper-playground.netlify.app");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         response.setHeader("Access-Control-Allow-Credentials", "true");
